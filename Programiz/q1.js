28bc63e2520699620e00b77d4563d530331679f2
nitinmmmut <115034091+nitinmmmut@users.noreply.github.com>
2023-08-26 23:21:12 +0530

    basic

---
+++ Programiz/q1.js
@@ -0,0 +1,23 @@
+// 1.using console.log
+
+// console.log("Hello World!");
+
+// using alert
+
+// alert("Hello World!");
+
+// 2.Add two Numbers
+
+// store input numbers
+
+// const num1 = parseInt(prompt('Enter the first number '));
+// const num2 = parseInt(prompt('Enter the second number '));
+
+const num1 = 2;
+const num2 = 5;
+
+//add two numbers
+const sum = num1 + num2;
+
+// display the sum
+console.log(`The sum of ${num1} and ${num2} is ${sum}`); //The sum of 2 and 5 is 7
