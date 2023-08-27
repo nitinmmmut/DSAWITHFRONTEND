d81d23b2ee9e9d1c7000c619a328ed5eceff8cde
nitinmmmut <115034091+nitinmmmut@users.noreply.github.com>
2023-08-27 20:17:41 +0530

    baisc

---
+++ Programiz/q2.js
@@ -0,0 +1,22 @@
+//sqaure root of a number
+
+// function squareRootOf(num) {
+//   const result = Math.sqrt(num);
+//   return result;
+// }
+
+// const number = 121;
+// const calResult = squareRootOf(number);
+// console.log(calResult);
+
+const number1 = 2.25;
+const number2 = -4;
+const number3 = "hello";
+
+const result1 = Math.sqrt(number1);
+const result2 = Math.sqrt(number2);
+const result3 = Math.sqrt(number3);
+
+console.log(`The square root of ${number1} is ${result1}`);
+console.log(`The square root of ${number2} is ${result2}`);
+console.log(`The square root of ${number3} is ${result3}`);
