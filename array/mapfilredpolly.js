99b48993170190f7678a1f4cf35476321bc84f83
nitinmmmut <115034091+nitinmmmut@users.noreply.github.com>
2023-09-09 22:27:56 +0530

    array

--- array/mapfilredpolly.js
+++ array/mapfilredpolly.js
@@ -32,3 +32,40 @@ if (!Array.prototype.myMap) {
 
 const myEmployeesName = employees.myMap((employee) => employee.name);
 console.log(myEmployeesName); //[ 'john', 'sarah', 'maichal' ]
+
+// Filter in javascript
+
+const products = [
+  { name: "iphone", price: 999, inStock: true },
+  { name: "Samsung", price: 899, inStock: false },
+  { name: "Google Pixel", price: 799, inStock: true },
+];
+
+// const availableProducts = products.filter((product) => product.inStock);
+
+const availableProducts = products.filter(function (product) {
+  return product.inStock === true;
+});
+
+console.log(availableProducts); //[{ name: 'iphone', price: 999, inStock: true },{ name: 'Google Pixel', price: 799, inStock: true }]
+
+// polyfill of filter
+if (!Array.prototype.myFilter) {
+  Array.prototype.myFilter = (callback) => {
+    const result = [];
+    for (let i = 0; i < this.length; i++) {
+      if (callback(this[i], i, this)) {
+        result.push(this[i]);
+      }
+    }
+    return result;
+  };
+}
+
+const myAvailableProducts = products.myFilter((product) => product.inStock);
+
+console.log(availableProducts); //o/p:
+// [
+//   { name: 'iphone', price: 999, inStock: true },
+//   { name: 'Google Pixel', price: 799, inStock: true }
+// ]
