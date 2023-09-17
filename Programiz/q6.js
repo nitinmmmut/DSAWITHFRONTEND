26b272c83eb7e760cf39c7593a8b149b442b4451
nitinmmmut <115034091+nitinmmmut@users.noreply.github.com>
2023-09-17 22:40:39 +0530

    basic

---
+++ Programiz/q6.js
@@ -0,0 +1,9 @@
+//JavaScript Program to Convert Kilometers to Miles
+
+const kiloMeters = 2.2;
+
+const factor = 0.621371;
+
+const miles = kiloMeters * factor;
+
+console.log(`${kiloMeters} km is equal to ${miles} miles.`);
