70c4c1714cad858a766e444d6e0a750dced9bcf0
nitinmmmut <115034091+nitinmmmut@users.noreply.github.com>
2023-09-20 23:34:56 +0530

    basic

---
+++ Programiz/q7.js
@@ -0,0 +1,11 @@
+//Javascript Program to Generate a Random Number
+const a = Math.random();
+
+console.log(a);
+
+//Example 2: Get a Random Number between 1 and 10
+const b = Math.random() * (10 - 1) + 1;
+console.log(b);
+
+const c = Math.floor(Math.random() * (10 - 1) + 1);
+console.log(c);
