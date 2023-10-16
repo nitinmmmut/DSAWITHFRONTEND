726704ab291ab0cd3a012a609671b1d68f93ac7c
nitinmmmut <115034091+nitinmmmut@users.noreply.github.com>
2023-10-16 23:34:33 +0530

    basic

---
+++ w3Resource/basic/Q.14/script.js
@@ -0,0 +1,11 @@
+function divideBy() {
+  num1 = document.getElementById("firstnumber").value;
+  num2 = document.getElementById("secondnumber").value;
+  document.getElementById("result").innerHTML = num1 / num2;
+}
+
+function multiplyBy() {
+  num1 = document.getElementById("firstnumber").value;
+  num2 = document.getElementById("secondnumber").value;
+  document.getElementById("result").innerHTML = num1 * num2;
+}
